71b35f39bb7b3d2a15539c95f2305a19aba3ecbc
himanshu singh <himanshus359@gmail.com>
2025-09-01 03:35:37 +0530

    modal update

---
+++ src/challenges/Counter/Modal.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import './Component.css'
+
+const Modal = () => {
+    const [showModal, setShowModal] = React.useState(false);
+    const [offerAccepted, setOfferAccepted] = React.useState(false)
+
+    const handleClick =()=>{
+      if(!offerAccepted) setShowModal(true)
+      
+    }
+
+    const handleofferButton=()=>{
+      setShowModal(false);
+      setOfferAccepted(true)
+    }
+    
+  return (
+    <>
+    
+    <div className='showButton'>
+      
+        <button type="button" onClick={handleClick}> {!offerAccepted ? 'Show' : 'Offer Accepted'} </button>
+    </div>
+    {showModal && 
+    <div className='modalClass'>
+      <div className='modalContent'>
+        <button className='cancelButton' onClick={()=>setShowModal(false)}> x </button>
+
+      <div className='content'>
+        Click the button below to accept our amazing offerdecccccccccccccccccccccccccccccccccccc.
+      </div>
+
+      <button className='offerButton' onClick={handleofferButton}>
+        Accept offer
+      </button>
+      </div>
+      
+    </div>
+
+    }
+
+    </>
+  )
+}
+
+export default Modal
